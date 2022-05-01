4c116c4ace7b1b83be170ce6360326cdbbe58fe8
radusofron <radu.sofron169@gmail.com>
2022-05-02 01:03:00 +0300

    Upload seeallposts.html; other updates
    Update darktheme, responsive design and structure.

--- Front-end files/scripts/darktheme.js
+++ Front-end files/scripts/darktheme.js
@@ -1,19 +1,5 @@
-/* scripts for responsive website */
-const menuBtn = document.querySelector('.menu-btn')
-const navlinks = document.querySelector('ul.right_container')
-menuBtn.addEventListener('click', () => {
-            navlinks.classList.toggle('mobile-menu')
-})
-
-const searchBtn = document.querySelector('.search-btn')
-const searchlink = document.querySelector('div.search_bar')
-searchBtn.addEventListener('click', () => {
-            searchlink.classList.toggle('search_menu')
-})
-
-
 /* change theme */
-const change_theme = document.getElementById("change_theme");
+const change_theme = document.getElementById("checkbox");
 
 change_theme.addEventListener('change',() => {
     
@@ -22,7 +8,7 @@ change_theme.addEventListener('change',() => {
     var index;
     var list;
     /* header content & footer */
-    list = document.querySelectorAll(".navig_line, .search_bar, .right_container, .latest, .categories, .about, .menu_option, .footer");
+    list = document.querySelectorAll(".navig_line, .search_bar, .site_name, .right_container, .latest, .categories, .about, .menu_option, .footer");
     for(index = 0; index < list.length; index++)
         list[index].classList.toggle('dark-theme');
     /* homepage content */
@@ -35,6 +21,14 @@ change_theme.addEventListener('change',() => {
     list = document.querySelectorAll(".most_reviewed_post, .twitter_post, .reddit_post, .youtube_post");
     for(index = 0; index < list.length; index++)
         list[index].classList.toggle('dark-theme-grey');
+    /* seeallposts content */
+    list = document.querySelectorAll(".platform_name, .platform");
+    for(index = 0; index < list.length; index++)
+        list[index].classList.toggle('dark-theme-dark-grey');
+    list = document.querySelectorAll(".platform_post .twitter_post, .platform_post .reddit_post, .platform_post .youtube_post");
+    for(index = 0; index < list.length; index++)
+        list[index].classList.toggle('dark-theme-grey');
+    /* homepage & seeallposts content */
     list = document.querySelectorAll(".title, .description");
     for(index = 0; index < list.length; index++)
         list[index].classList.toggle('dark-theme-light-grey');
