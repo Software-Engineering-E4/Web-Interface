53df608587355f7e78c4795e26265233aec54256
radusofron <radu.sofron169@gmail.com>
2022-05-09 19:17:12 +0300

    Upload redditpost.html. Update twitterpost.html.
    Dark theme added.
    href change dinamically

--- Front-end files/scripts/seeallposts.js
+++ Front-end files/scripts/seeallposts.js
@@ -4,11 +4,13 @@ var option = optionFromLocalStorage;
 
 var twitter = 'twitter';
 if(option === twitter) {
+    /* add right title */
     document.getElementById("platform_name").innerHTML = 'Twitter';
     var elem;
     elem = document.querySelector(".platform_name");
     elem.classList.toggle('twitter');
 
+    /* add right styled posts */
     var list;
     list = document.querySelectorAll(".platform");
     for(index = 0; index < list.length; index++)
@@ -16,6 +18,11 @@ if(option === twitter) {
     list = document.querySelectorAll(".platform_post");
     for(index = 0; index < list.length; index++)
         list[index].classList.toggle('twitter_post');
+
+    /* add right href attributes */
+    var a = document.querySelectorAll(".post");
+    for(index = 0; index < a.length; index++)
+        a[index].setAttribute("href", "twitterpost.html");
 }
 var reddit = 'reddit';
 if(option === reddit) {
@@ -31,6 +38,10 @@ if(option === reddit) {
     list = document.querySelectorAll(".platform_post");
     for(index = 0; index < list.length; index++)
         list[index].classList.toggle('reddit_post');
+
+    var a = document.querySelectorAll(".post");
+    for(index = 0; index < a.length; index++)
+        a[index].setAttribute("href", "redditpost.html");
 }
 var youtube = 'youtube';
 if(option === youtube) {
